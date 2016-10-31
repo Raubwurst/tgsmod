8ae715382b86dea4eed71ea266a8627c14a63a05
Richard Khulusi <raubwurst@web.de>
2016-10-31 15:37:22 +0100

    Changed Blight terrain from snow to dead-grass-like

--- module_info.py
+++ module_info.py
@@ -1,6 +1,6 @@
 
 # Point export_dir to the folder you will be keeping your module
 # Make sure you use forward slashes (/) and NOT backward slashes (\)
-
-export_dir = "C:/Program Files (x86)/Mount&Blade Warband/Modules/WoT_Mod/"
+export_dir = "D:/Program Files/Steam/SteamApps/common/MountBlade Warband/Modules/The Gathering Storm WIP/"
+#export_dir = "C:/Program Files (x86)/Mount&Blade Warband/Modules/WoT_Mod/"
 #export_dir = "C:/Program Files/Mount&Blade/Modules/Native/"
